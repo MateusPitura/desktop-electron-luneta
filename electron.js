85083e4ba3a05a9884ad7ee4833b786ee369f7b1
MateusPitura <mateuspitura@gmail.com>
2024-12-12 21:54:08 -0300

    build: added initial config

---
+++ electron.js
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+//   mainWindow.loadURL("http://localhost:5173"); // Dev
+  mainWindow.loadFile(path.join(__dirname, "dist/index.html")); // Prod
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
