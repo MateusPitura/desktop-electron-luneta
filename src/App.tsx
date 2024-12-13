81cb707e7395b0660938b10dd4739a21ae8f1e79
MateusPitura <mateuspitura@gmail.com>
2024-12-12 22:34:37 -0300

    refactor: app initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function App() {
-  return <div>Hello, world</div>;
+  return (
+    <div className="p-4 bg-neutral-300 flex">
+      <label className="flex flex-1 gap-2">
+        <div className="place-items-center">
+          <SearchIcon fontSize="large"/>
+        </div>
+        <input type="search" className="flex-1 p-2 outline-none appearance-none" />
+      </label>
+    </div>
+  );
 }
