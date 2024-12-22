ca309196b3ab7263082cefdd4afd844d6db9ec95
MateusPitura <mateuspitura@gmail.com>
2024-12-22 20:55:30 -0300

    fix: fix bug that removes the filter when enter

--- src/components/FilterActive.tsx
+++ src/components/FilterActive.tsx
@@ -26,12 +26,12 @@ export default function FilterActive({
         value={option.value}
       />
       <span className="text-dark-onSecondary text-sm">{option.label}</span>
-      <button
+      <div
         className="text-dark-onSecondary flex items-center hover:opacity-50"
         onClick={() => onClick(option)}
       >
         <Clear color="inherit" fontSize="small" />
-      </button>
+      </div>
     </label>
   );
 }
