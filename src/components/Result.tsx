d03351ea19300c896b0fb3bbc015ef306596b84c
MateusPitura <mateuspitura@gmail.com>
2024-12-23 19:37:23 -0300

    refactor: displaying info even when switch sections

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState, type ReactElement } from "react";
-import { Execute } from "../utils/types";
+import { type ReactElement } from "react";
 import ResultWrapper from "./ResultWrapper";
 
 export default function Result(): ReactElement {
-  const [result, setResult] = useState<Execute>();
-
-  useEffect(() => {
-    window.electron.receive("result", (data) => {
-      setResult(JSON.parse(data));
-    });
-  }, []);
-
   return (
     <div className="flex flex-1 overflow-auto">
-      <ResultWrapper result={result} />
+      <ResultWrapper />
     </div>
   );
 }
--- src/components/ResultWrapper.tsx
+++ src/components/ResultWrapper.tsx
@@ -1,24 +1,34 @@
-import { useContext, type ReactElement } from "react";
+import { useContext, useEffect, useState, type ReactElement } from "react";
 import { Execute, Sections } from "../utils/types";
 import ResultTranslate from "./ResultTranslate";
 import ResultDictionary from "./ResultDictionary";
 import { GlobalContext } from "../GlobalContext";
 
-interface ResultWrapperProperties {
-  result?: Execute;
+interface ExecuteOutput {
+  Translate?: Execute;
+  Dictionary?: Execute;
 }
 
-export default function ResultWrapper({
-  result,
-}: ResultWrapperProperties): ReactElement {
+export default function ResultWrapper(): ReactElement {
   const { currentSectionActive } = useContext(GlobalContext);
+  const [result, setResult] = useState<ExecuteOutput>();
+
+  useEffect(() => {
+    window.electron.receive("result", (data) => {
+      const dataFormatted: Execute = JSON.parse(data);
+      setResult((prev) => ({
+        ...prev,
+        [dataFormatted.section]: dataFormatted,
+      }));
+    });
+  }, []);
 
   switch (currentSectionActive) {
     case Sections.TRANSLATE:
-      return <ResultTranslate result={result} />;
+      return <ResultTranslate result={result?.Translate} />;
     case Sections.DICTIONARY:
-      return <ResultDictionary result={result}/>;
+      return <ResultDictionary result={result?.Dictionary} />;
     default:
-      return <ResultTranslate result={result} />;
+      return <ResultTranslate result={result?.Translate} />;
   }
 }
